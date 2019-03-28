9339ce5e07d5e92775ab9df2b0e61afe7ccb403d
YING CHEN <ying.chen@naamtech.com>
2019-03-28 21:05:35 +1100

    comment out unnecessary codes.

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -35,18 +35,18 @@
                         <i class="ni ni-single-02"></i>
                         <span>{{ __('My profile') }}</span>
                     </a>
-                    <a href="#" class="dropdown-item">
-                        <i class="ni ni-settings-gear-65"></i>
-                        <span>{{ __('Settings') }}</span>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <i class="ni ni-calendar-grid-58"></i>
-                        <span>{{ __('Activity') }}</span>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <i class="ni ni-support-16"></i>
-                        <span>{{ __('Support') }}</span>
-                    </a>
+                    {{--<a href="#" class="dropdown-item">--}}
+                        {{--<i class="ni ni-settings-gear-65"></i>--}}
+                        {{--<span>{{ __('Settings') }}</span>--}}
+                    {{--</a>--}}
+                    {{--<a href="#" class="dropdown-item">--}}
+                        {{--<i class="ni ni-calendar-grid-58"></i>--}}
+                        {{--<span>{{ __('Activity') }}</span>--}}
+                    {{--</a>--}}
+                    {{--<a href="#" class="dropdown-item">--}}
+                        {{--<i class="ni ni-support-16"></i>--}}
+                        {{--<span>{{ __('Support') }}</span>--}}
+                    {{--</a>--}}
                     <div class="dropdown-divider"></div>
                     <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -26,18 +26,18 @@
                         <i class="ni ni-single-02"></i>
                         <span>{{ __('My profile') }}</span>
                     </a>
-                    <a href="#" class="dropdown-item">
-                        <i class="ni ni-settings-gear-65"></i>
-                        <span>{{ __('Settings') }}</span>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <i class="ni ni-calendar-grid-58"></i>
-                        <span>{{ __('Activity') }}</span>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <i class="ni ni-support-16"></i>
-                        <span>{{ __('Support') }}</span>
-                    </a>
+                    {{--<a href="#" class="dropdown-item">--}}
+                        {{--<i class="ni ni-settings-gear-65"></i>--}}
+                        {{--<span>{{ __('Settings') }}</span>--}}
+                    {{--</a>--}}
+                    {{--<a href="#" class="dropdown-item">--}}
+                        {{--<i class="ni ni-calendar-grid-58"></i>--}}
+                        {{--<span>{{ __('Activity') }}</span>--}}
+                    {{--</a>--}}
+                    {{--<a href="#" class="dropdown-item">--}}
+                        {{--<i class="ni ni-support-16"></i>--}}
+                        {{--<span>{{ __('Support') }}</span>--}}
+                    {{--</a>--}}
                     <div class="dropdown-divider"></div>
                     <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
@@ -83,58 +83,58 @@
                         <i class="ni ni-tv-2 text-primary"></i> {{ __('Dashboard') }}
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-examples">
-                        <i class="fab fa-laravel"></i>
-                        <span class="nav-link-text">{{ __('Laravel Examples') }}</span>
-                    </a>
+                {{--<li class="nav-item">--}}
+                    {{--<a class="nav-link" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-examples">--}}
+                        {{--<i class="fab fa-laravel"></i>--}}
+                        {{--<span class="nav-link-text">{{ __('Laravel Examples') }}</span>--}}
+                    {{--</a>--}}
 
-                    <div class="collapse " id="navbar-examples">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('profile.edit') }}">
-                                    {{ __('User profile') }}
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.index') }}">
-                                    {{ __('User Management') }}
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
+                    {{--<div class="collapse " id="navbar-examples">--}}
+                        {{--<ul class="nav nav-sm flex-column">--}}
+                            {{--<li class="nav-item">--}}
+                                {{--<a class="nav-link" href="{{ route('profile.edit') }}">--}}
+                                    {{--{{ __('User profile') }}--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                            {{--<li class="nav-item">--}}
+                                {{--<a class="nav-link" href="{{ route('user.index') }}">--}}
+                                    {{--{{ __('User Management') }}--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                        {{--</ul>--}}
+                    {{--</div>--}}
+                {{--</li>--}}
                 <li class="nav-item @yield('projects-active')">
                     <a class="nav-link" href="{{ route('project.index') }}">
                         <i class="ni ni-spaceship text-blue"></i> {{ __('Solar Projects') }}
                     </a>
                 </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="ni ni-planet text-blue"></i> {{ __('Icons') }}
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="ni ni-pin-3 text-orange"></i> {{ __('Maps') }}
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="ni ni-key-25 text-info"></i> {{ __('Login') }}
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="ni ni-circle-08 text-pink"></i> {{ __('Register') }}
-                    </a>
-                </li>
-                <li class="nav-item mb-5" style="position: absolute; bottom: 0;">
-                    <a class="nav-link" href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank">
-                        <i class="ni ni-cloud-download-95"></i> Upgrade to PRO
-                    </a>
-                </li>
+                {{--<li class="nav-item">--}}
+                    {{--<a class="nav-link" href="#">--}}
+                        {{--<i class="ni ni-planet text-blue"></i> {{ __('Icons') }}--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li class="nav-item">--}}
+                    {{--<a class="nav-link" href="#">--}}
+                        {{--<i class="ni ni-pin-3 text-orange"></i> {{ __('Maps') }}--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li class="nav-item">--}}
+                    {{--<a class="nav-link" href="#">--}}
+                        {{--<i class="ni ni-key-25 text-info"></i> {{ __('Login') }}--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li class="nav-item">--}}
+                    {{--<a class="nav-link" href="#">--}}
+                        {{--<i class="ni ni-circle-08 text-pink"></i> {{ __('Register') }}--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li class="nav-item mb-5" style="position: absolute; bottom: 0;">--}}
+                    {{--<a class="nav-link" href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank">--}}
+                        {{--<i class="ni ni-cloud-download-95"></i> Upgrade to PRO--}}
+                    {{--</a>--}}
+                {{--</li>--}}
             </ul>
             <!-- Divider -->
             <hr class="my-3">
